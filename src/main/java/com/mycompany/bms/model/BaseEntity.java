4e0800175520e45c3e70343ee50700d1d0d9138a
Malina Shakya <126803519+malinashakya@users.noreply.github.com>
2024-08-15 12:15:05 +0545

    Created and Updated At inserted in GenericModel

--- src/main/java/com/mycompany/bms/model/BaseEntity.java
+++ src/main/java/com/mycompany/bms/model/BaseEntity.java
@@ -10,12 +10,18 @@ package com.mycompany.bms.model;
  * @author malina
  */
 import java.io.Serializable;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 
 @MappedSuperclass
+//Designates a class as a base class whose properties are inherited by its subclasses but not directly mapped to a database table.
+
 public abstract class BaseEntity implements Serializable{
     
     @Id
@@ -30,6 +36,41 @@ public abstract class BaseEntity implements Serializable{
         this.id = id;
     }
         
+    @Column(name="created_at", updatable=false)
+    private LocalDateTime createdAt;
+    
+    @Column(name="updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+//Annotates a method to be called automatically before a new entity is inserted into the database.
+    protected void onCreate()
+    {
+        createdAt=LocalDateTime.now();
+        updatedAt=LocalDateTime.now();
+    }
+    
+    @PreUpdate
+//Annotates a method to be called automatically before an entity is updated in the database.
+    protected void onUpdate()
+    {
+        updatedAt=LocalDateTime.now();
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
 
-//Entity kahile crated ra updated vayeko vanera chahiyo, database ma store garne kahile create vayeko kahile update vayeko generic bata
